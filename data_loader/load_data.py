0ca1e517550640f206674eb44462613c55403f34
Marek <marecki.mail@gmail.com>
2021-05-19 12:30:30 +0200

    relations

--- data_loader/load_data.py
+++ data_loader/load_data.py
@@ -1,19 +1,18 @@
-import sys
 from time import time
-from get_data import read_clear_movies, read_rating, read_users;
+from get_data import read_clear_movies, read_rating, read_users
 from models import User, Movie, Rating
 from database import db_session, db_base, db_engine
-db_base.metadata.create_all(bind=db_engine)
 
+# Apply migrations to db and populate it
+db_base.metadata.create_all(bind=db_engine)
 now = time()
 
 ### USERS
-try:
-    ses = db_session()
-    df = read_users()
-    data = df.values
-
-    for i in data:
+(row_succes, row_error) = (0,0)
+ses = db_session()
+data = read_users().values
+for i in data:
+    try:
         record = User(**{
             'id': i[0],
             'full_name': i[1],
@@ -21,25 +20,22 @@ try:
             'hashed_password': i[3],
             'is_active': i[4]
         })
-        ses.add(record) #Add all the records
-
-    ses.commit() #Attempt to commit all the records
-    print("User commited");
-except:
-    ses.rollback() #Rollback the changes on error
-    print("User exception:", sys.exc_info()[1])
-finally:
-    ses.close() #Close the connection
-    print("User session close\n");
-
+        ses.add(record)
+        ses.commit()
+        row_succes += 1
+    except:
+        ses.rollback()
+        ses = db_session()
+        row_error += 1
+ses.close()
+print(f"User result: succes: {row_succes}, faild: {row_error}")
 
 ### MOVIES
-try:
-    ses = db_session()
-    data = read_clear_movies()
-    data = data.values
-
-    for i in data:
+(row_succes, row_error) = (0,0)
+ses = db_session()
+data = read_clear_movies().values
+for i in data:
+    try:
         record = Movie(**{
             'id': i[0],
             'imdb_id': i[1],
@@ -49,39 +45,63 @@ try:
             'overview': i[5]
         })
         ses.add(record)
-
-    ses.commit()
-    print("Movie commited");
-except:
-    ses.rollback()
-    print("Movie exception:", sys.exc_info()[1])
-finally:
-    ses.close()
-    print("Movie session close\n");
+        ses.commit()
+        row_succes += 1
+    except:
+        ses.rollback()
+        ses = db_session()
+        row_error += 1
+ses.close()
+print(f"Movie result: succes: {row_succes}, faild: {row_error}")
 
 
 ### RATING
-try:
-    ses = db_session()
-    data = read_rating()
-    data = data.values
-
-    for i in data:
+(row_succes, row_error) = (0,0)
+ses = db_session()
+data = read_rating().values
+for i in data:
+    try:
         record = Rating(**{
-        'user_id': i[0],
-        'movie_id': i[1],
-        'rating': i[2],
-        'time_stamp': i[3]
+            'user_id': i[0],
+            'movie_id': i[1],
+            'rating': i[2],
+            'time_stamp': i[3]
         })
         ses.add(record)
-
-    ses.commit()
-    print("Rating commited");
-except:
-    ses.rollback()
-    print("Rating exception:", sys.exc_info()[1])
-finally:
-    ses.close() #Close the connection
-    print("Rating session close\n")
+        ses.commit()
+        row_succes += 1
+    except:
+        row_error += 1
+        ses.rollback()
+        ses = db_session()
+ses.close()
+print(f"Rating result: succes: {row_succes}, faild: {row_error}")
 
 print("Time elapsed: " + str(time() - now) + " s.")
+
+
+# MOVIE COPY
+# try:
+#     ses = db_session()
+#     data = read_clear_movies()
+#     data = data.values
+#
+#     for i in data:
+#         record = Movie(**{
+#             'id': i[0],
+#             'imdb_id': i[1],
+#             'title': i[2],
+#             'genres': i[3],
+#             'release_date': i[4],
+#             'overview': i[5]
+#         })
+#         ses.add(record)
+#
+#     ses.commit()
+#     print("Movie commited");
+# except:
+#     ses.rollback()
+#     print("Movie exception:", sys.exc_info()[1])
+# finally:
+#     ses.close()
+#     print("Movie session close\n");
--- data_loader/models.py
+++ data_loader/models.py
@@ -1,18 +1,33 @@
-from sqlalchemy import Column, Integer, Float, Date, String, TIMESTAMP, Boolean
+from sqlalchemy import Column, Integer, Float, Date, String, TIMESTAMP, Boolean, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
 from database import db_base
 
 class User(db_base):
-    __tablename__ = "User"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String)
     email = Column(String) #, unique=True, index=True
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-    # rates = relationship("Rating")
+
+    rates = relationship("Rating", back_populates="owner")
+
+
+class Rating(db_base):
+    __tablename__ = 'ratings'
+    __table_args__ = (UniqueConstraint('user_id', 'movie_id'),)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    movie_id = Column(Float, ForeignKey("movies.id"))
+    rating = Column(Float)
+    time_stamp = Column(String)
+
+    owner = relationship("User", back_populates="rates")
+    to = relationship("Movie", back_populates="rates")
+
 
 class Movie(db_base):
-    __tablename__ = 'Movie'
+    __tablename__ = 'movies'
     id = Column(Integer, primary_key=True)
     imdb_id = Column(String)
     title = Column(String)
@@ -20,11 +35,4 @@ class Movie(db_base):
     release_date = Column(Date)
     overview = Column(String)
 
-class Rating(db_base):
-    __tablename__ = 'Rating'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer)
-    # user_id = relationship("User")
-    movie_id = Column(Float)
-    rating = Column(Float)
-    time_stamp = Column(String)
+    rates = relationship("Rating", back_populates="to")
